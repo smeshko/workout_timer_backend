962e2c4cbc62c669732dbbea3840fc94f7cb05bd
Ivaylo Tsonev <itsonev@ebay.com>
2020-05-26 21:49:57 +0200

    Update giitignore

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,10 +6,9 @@ import Liquid
 import LiquidLocalDriver
 
 extension Environment {
-    static let pgUrl = URL(string: Self.get("DB_URL")!)!
-    static let appUrl = URL(string: Self.get("APP_URL")!)!
-    static let awsKey = Self.get("AWS_KEY")!
-    static let awsSecret = Self.get("AWS_SECRET")!
+    static var staging: Environment {
+        .custom(name: "staging")
+    }
 }
 
 // Called before your application initializes.
@@ -17,17 +16,18 @@ public func configure(_ app: Application) throws {
     // Serves files from `Public/` directory
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.routes.defaultMaxBodySize = "10mb"
-
+    
     app.fileStorages.use(.local(publicUrl: "http://localhost:8080",
                                 publicPath: app.directory.publicDirectory,
                                 workDirectory: "assets"), as: .local)
     
-    // Configure SQLite database
-//    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
-//    try app.databases.use(.postgres(url: Environment.pgUrl), as: .psql)
-    app.databases.use(.postgres(hostname: "localhost", username: "smeshko", password: "darkknight1", database: "workout_timer"), as: .psql)
-
-
+    switch app.environment {
+    case .production, .development, .staging:
+        app.databases.use(try .postgres(url: Environment.get("DATABASE_URL") ?? ""), as: .psql)
+    default: break
+    }
+    
+    
     
     // Configure modules
     let modules: [Module] = [
